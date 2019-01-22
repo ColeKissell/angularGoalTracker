f9e63c642190b54ae6ba272657b859c94c168601
Nicole Kissell <colekissell@gmail.com>
2019-01-21 17:40:50 -0700

    todolist works

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
+import gql from 'graphql-tag';
+import { Todo, Query } from "../types";
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
